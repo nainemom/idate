e27113d58a410365528e33caa97d4f95df949806
nainemom <nainemom@gmail.com>
2019-10-24 16:24:17 +0330

    updated

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const MyDate = require('./dist/idate').JalaliDate
-
-const dt = new MyDate()
-dt.setMonth(0)
+const MyDate = require('./dist/hijri')
+// const baseDate = new Date('29 oct 2019')
+const dt = new MyDate(1440, 6, 23) // 23 rajab(06) 1440, 10 farv(00) 98, 30 march(02) 2019
+const dt2 = new MyDate(1440, 6, 24)
+// dt.setMonth(0)
 console.log(dt.toString())
+console.log(dt.di.toString())
+console.log('------------------')
+console.log(dt2.toString())
+console.log(dt2.di.toString())
