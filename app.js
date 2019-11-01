bf03f70db5723ec5abe7764c4657d4044da105ce
Amir Momenian <nainemom@gmail.com>
2019-11-01 22:50:00 +0330

    update test file

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const MyDate = require('./dist/hijri')
+const JalaliCalendar = require('./dist/jalali')
+const IDate = require('./dist/idate')
+
+const MyDate = IDate(JalaliCalendar)
 // const baseDate = new Date('29 oct 2019')
-const dt = new MyDate(1440, 6, 23) // 23 rajab(06) 1440, 10 farv(00) 98, 30 march(02) 2019
-const dt2 = new MyDate(1440, 6, 24)
-// dt.setMonth(0)
-console.log(dt.toString())
-console.log(dt.di.toString())
-console.log('------------------')
-console.log(dt2.toString())
-console.log(dt2.di.toString())
+// const dt = new MyDate(1440, 6, 23) // 23 rajab(06) 1440, 10 farv(00) 98, 30 march(02) 2019
+// const dt2 = new MyDate(1440, 6, 24)
+// // dt.setMonth(0)
+// console.log(dt.toString())
+// console.log(dt.di.toString())
+// console.log('------------------')
+// console.log(dt2.toString())
+// console.log(dt2.di.toString())
+
+
+console.log(new MyDate().toString())
